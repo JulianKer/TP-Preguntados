a827ac141ed9fd7f293806eaca7a8289bba311a7
GermanSchmuker <germanschmuker@gmail.com>
2024-11-10 23:48:29 -0300

    mostrar perfil de usuario con QR terminado (podé acceder sin estar logueado)

---
+++ helper/QrGenerador.php
@@ -0,0 +1,12 @@
+<?php
+
+class QrGenerador{
+    public static function generarYguardarQr($contenidoDelQr){
+        $generator = new barcode_generator();
+
+        $svg = $generator->render_svg("qr", $contenidoDelQr, "");
+        $filePath = $_SERVER['DOCUMENT_ROOT'] . "/public/images/qr/qr.svg";
+        file_put_contents($filePath, $svg);
+        //echo $svg;
+    }
+}
