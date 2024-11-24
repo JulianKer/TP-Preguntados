6291f13b2d145c660d3d52d695c600f6842733e2
JulianKer <jschmuker742@alumno.unlam.edu.ar>
2024-11-24 17:41:33 -0300

    Correccion SEGURIDAD instanciada en el router + switch segun estado (denegado, autorizado o no logueado)

--- helper/Router.php
+++ helper/Router.php
@@ -5,22 +5,21 @@ class Router
     private $defaultController;
     private $defaultMethod;
     private $configuration;
+    private $seguridad;
 
-    public function __construct($configuration, $defaultController, $defaultMethod)
-    {
+    public function __construct($configuration, $defaultController, $defaultMethod, $seguridad){
         $this->defaultController = $defaultController;
         $this->defaultMethod = $defaultMethod;
         $this->configuration = $configuration;
+        $this->seguridad = $seguridad;
     }
 
-    public function route($controllerName, $methodName)
-    {
+    public function route($controllerName, $methodName){
         $controller = $this->getControllerFrom($controllerName);
         $this->executeMethodFromController($controller, $methodName);
     }
 
-    private function getControllerFrom($module)
-    {
+    private function getControllerFrom($module){
         $controllerName = 'get' . ucfirst($module) . 'Controller';
         $validController = method_exists($this->configuration, $controllerName) ? $controllerName : $this->defaultController;
 
@@ -36,6 +35,24 @@ class Router
     }
 
     private function executeMethodFromController($controller, $method){
+
+        $validMethod = method_exists($controller, $method) ? $method : $this->defaultMethod;
+
+        switch ($this->seguridad->verificarPermisosParaEsteMetodo($validMethod)){
+            case "AUTORIZADO": // si esta autorizado, tod ok y te dejo entrar
+                call_user_func(array($controller, $validMethod));
+                break;
+            case "DENEGADO": // si no tenes permisos, te mando al default que es el principal/default (default  -> inicio)
+                header("location: /principal/" . $this->defaultMethod);
+                exit();
+            case "NO LOGUEADO": // si no esta logueado, te mando al ingresar directamente
+                header("location: /acceso/ingresar");
+                exit();
+        }
+
+
+        /*   ANTES EL PROFE NOS DIJO Q ESTO SE PODIA HACER CON EL SWITCH y lo hice pero lo dejo para q vea
+
         $metodosPublicos = parse_ini_file("permisosUsuarios.ini", true)['metodosPublicos'];
 
         $validMethod = method_exists($controller, $method) ? $method : $this->defaultMethod;
@@ -49,6 +66,6 @@ class Router
                 Seguridad::verSiHayUnUsuarioEnLaSesion();
                 call_user_func(array($controller, $this->defaultMethod));
             }
-        }
+        }*/
     }
 }
--- helper/Seguridad.php
+++ helper/Seguridad.php
@@ -2,7 +2,44 @@
 
 class Seguridad{
 
-    public static function verificarSiTienePermisos($metodoQueQuiereEjecutar){
+    private $metodosPublicos;
+
+    private $metodosSeguros;
+
+    public function __construct(){
+        $this->metodosPublicos = parse_ini_file("permisosUsuarios.ini", true)['metodosPublicos'];
+        $this->metodosSeguros = parse_ini_file("permisosUsuarios.ini", true)['metodosSeguros'];
+    }
+
+    public function verificarPermisosParaEsteMetodo($metodoAValidarPermisos){
+        // 1° ver si al q quiere ingresar si es un metodo publico, si es publico, ENTRá nomas
+        // 2° si NO es publico, ver si está logueado, si NO esta logueado, te mando al acceso/ingresar
+        // 3° si ESTA logueado, veo si tiene permisos, sino te mando al principal/inicio
+
+        if (array_key_exists($metodoAValidarPermisos, $this->metodosPublicos)) {
+            return "AUTORIZADO"; // si quiere acceder a un publico, te dejo pq no me interesa si esta logueado ni si tiene permisos pq justamente es PUBLICO
+        }
+
+        if (!isset($_SESSION['usuarioObjetoDeLaSesion'])){
+            return "NO LOGUEADO";
+        }
+
+        $rangoDelUser = $_SESSION['usuarioObjetoDeLaSesion']['rango'];
+        if (isset($this->metodosSeguros[$metodoAValidarPermisos])){
+            $rangosPermitidosParaEseMetodo = explode(',', $this->metodosSeguros[$metodoAValidarPermisos]); // --> inicio=1,2,3 --> [1, 2, 3]
+
+            if (in_array($rangoDelUser, $rangosPermitidosParaEseMetodo)){
+                return "AUTORIZADO";
+            }
+        }
+        return "DENEGADO";
+    }
+
+
+
+
+
+    /*public static function verificarSiTienePermisos($metodoQueQuiereEjecutar){
         $usuarioDeLaSesion = isset($_SESSION['usuarioObjetoDeLaSesion']) ? $_SESSION['usuarioObjetoDeLaSesion'] : null;
         $rangoDelUser = 0;
 
@@ -27,5 +64,5 @@ class Seguridad{
             header("location: /acceso/ingresar");
             exit();
         }
-    }
+    }*/
 }
